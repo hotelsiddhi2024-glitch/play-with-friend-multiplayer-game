f3dba5cd376d5a02e47b0075ae96952f4f54d122
hotelsiddhi2024-glitch <hotelsiddhi2024@gmail.com>
2025-08-05 04:04:53 +0530

    Create button.tsx

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button: React.FC<ButtonProps> = ({ children, className = '', ...props }) => (
+  <button className={`px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 ${className}`} {...props}>
+    {children}
+  </button>
+)
+
+export default Button

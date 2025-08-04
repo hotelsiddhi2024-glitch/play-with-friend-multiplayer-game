8dcf34a20bbeffcb416315352abda4295da57be4
hotelsiddhi2024-glitch <hotelsiddhi2024@gmail.com>
2025-08-05 03:23:10 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1754344390562

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7ca2b4d7._.js",
+  "static/chunks/node_modules_ce3b012a._.js"
+],
+    source: "dynamic"
+});
